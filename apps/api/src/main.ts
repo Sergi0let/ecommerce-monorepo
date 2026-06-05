e4df9a2537a948af5e843cb6e59ed8fa8cbbdff9
Sergi0let <s.vashkevych@gmail.com>
2026-06-05 18:45:49 +0300

    feat(api): validation-pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExeptionFilter } from './common/filters/prisma.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaExeptionFilter());
 
   await app.listen(process.env.PORT ?? 3006);
