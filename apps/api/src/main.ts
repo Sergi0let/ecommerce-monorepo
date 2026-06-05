f1b653bff9694e90d734f07a9ae5c69f782e2c32
Sergi0let <s.vashkevych@gmail.com>
2026-06-05 18:41:14 +0300

    feat(api): add Prisma Exception filter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaExeptionFilter } from './common/filters/prisma.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new PrismaExeptionFilter());
+
   await app.listen(process.env.PORT ?? 3006);
 }
 void bootstrap();
