b01a5bf625eabb9616e6bedb5f010f0bc67ab128
Sergi0let <s.vashkevych@gmail.com>
2026-06-06 11:07:46 +0300

    feat(api): add api prefix, express type way

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,13 +3,17 @@ import { AppModule } from './app.module';
 import { PrismaExeptionFilter } from './common/filters/prisma.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
 
   app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaExeptionFilter());
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3006);
 }
