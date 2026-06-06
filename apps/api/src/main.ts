19720aac4892b6a2106353bb31c2301105e9827b
Sergi0let <s.vashkevych@gmail.com>
2026-06-06 15:17:14 +0300

    feat(api): add Swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,10 +4,23 @@ import { PrismaExeptionFilter } from './common/filters/prisma.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Market Cosmo API')
+    .setDescription('Market Cosmo API description')
+    .setVersion('1.0')
+    .addTag('market-cosmo')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
   });
 
   app.useLogger(app.get(Logger));
